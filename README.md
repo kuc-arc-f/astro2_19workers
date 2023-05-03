59cd9c0f3e5ee6a9e9f600ecc9f124b07e2cb1f4
中島浩二 <knaka0209@gmail.com>
2023-05-04 08:42:48 +0900

    up, 0504a2

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
  date    : 2023/05/02
 
- update  : 
+ update  : 2023/05/04
 
 ***
 ### Summary
--- server/src/Router.ts
+++ server/src/Router.ts
@@ -1,4 +1,5 @@
 import testRouter from './routes/test';
+import todoRouter from './routes/todo';
 //
 const Router = {
   /**
@@ -10,6 +11,7 @@ const Router = {
   route: async function (pathname: string, req: any, res: any, env: any): Promise<Response>
   {
     let response;
+    /* test */
     if (pathname === "/test2") {
       response = await testRouter.test2(req, res, env);
     }
@@ -30,6 +32,23 @@ const Router = {
     }
     if (pathname === "/test/update") {
       response = await testRouter.update(req, res, env);  
+    }
+    //todo
+    // create
+    if (pathname === "/todos/create") {
+      response = await todoRouter.create(req, res, env);  
+    }
+    if (pathname === "/todos/delete") {
+      response = await todoRouter.delete(req, res, env);  
+    }
+    if (pathname === "/todos/update") {
+      response = await todoRouter.update(req, res, env);  
+    }
+    if (pathname === "/todos/get") {
+      response = await todoRouter.get(req, res, env);  
+    }
+    if (pathname === "/todos/get_list") {
+      response = await todoRouter.get_list(req, res, env);  
     }
     //@ts-ignore
     return response;
